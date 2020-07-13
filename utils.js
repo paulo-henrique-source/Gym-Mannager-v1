c7266545523582085a20c400e47c5bc35d7f78a4
paulo-henrique-source <paulohenrique1117@hotmail.com>
2020-07-13 20:23:58 -0300

    Gym Mannager V1

---
+++ utils.js
@@ -0,0 +1,41 @@
+module.exports = {
+    age: function name(timestamp) {
+
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+
+        const month = today.getFullYear() - birthDate.getFullYear()
+
+
+
+        if (month < 0 || month == 0 && today.getDate() <= birthDate.getDate()) {
+            age = age - 1
+        }
+
+        return age
+    },
+    date: function (timestamp) {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        
+        
+        const day = `0${date.getUTCDate()}`.slice(-2)
+        
+        return {
+        day,
+        month,
+        year, 
+        iso:`${year}-${month}-${day}`,
+        birthDay: `${day}/${month}`
+
+        }
+    }
+
+}
